96cb5ddf90fee930986562e3c6b4795fbbcffadc
Ali Karabulut <a.kbt6666@gmail.com>
2023-08-06 02:18:24 +0300

    login page without image slider animation

--- src/components/login/homeSlider/index.tsx
+++ src/components/login/homeSlider/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const HomeSlider = () => {
   return (
     <div
-      className="bg-cover bg-center h-[635px] w-[385px] relative "
+      className="bg-cover bg-center h-[635px] w-[430px] relative mr-8 "
       style={{ backgroundImage: "url(/img/home-phones.png)" }}
     >
       <Image
@@ -11,21 +11,21 @@ const HomeSlider = () => {
         height={541}
         width={250}
         alt="screenshot1"
-        className="absolute top-[30px] right-[19px]"
+        className="absolute top-[30px] right-[24px] animate-opacity z-[-1]"
       ></Image>
       <Image
         src={"/img/screenshot2.png"}
         height={541}
         width={250}
         alt="screenshot2"
-        className="absolute top-[30px] right-[19px]"
+        className="absolute top-[30px] right-[24px] animate-opacity delay-4s z-[-1] "
       ></Image>
       <Image
         src={"/img/screenshot3.png"}
         height={541}
         width={250}
         alt="screenshot3"
-        className="absolute top-[30px] right-[19px]"
+        className="absolute top-[30px] right-[24px] animate-opacity delay-8s z-[-1]"
       ></Image>
     </div>
   );
