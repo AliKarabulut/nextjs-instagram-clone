4c47f92e29c456db9789cd4dc0776890489c297a
Ali Karabulut <a.kbt6666@gmail.com>
2023-08-05 17:31:30 +0300

    input component

--- src/components/layouts/loginFormLayout/page.tsx
+++ src/components/layouts/loginFormLayout/page.tsx
@@ -1,18 +1,23 @@
+"use client"
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 type LayoutTypes = {
   children: React.ReactNode;
 };
 
 const LoginFormLayout = ({ children }: LayoutTypes) => {
+  const params = useParams();
   return (
-    <div className="w-[350px] h-fit ">
+    <div className="w-[350px] h-fit flex justify-center flex-col">
       <div className="border-[1px]  ">
-        <div
-          className="w-44 h-14 bg-no-repeat  bg-logo bg-pos"
-          style={{ backgroundImage: "url(/img/instagramLogo.png)" }}
-        ></div>
-        <form action="" className="flex items-center  flex-col">
+        <div className="w-full flex justify-center py-8">
+          <div
+            className="w-44 h-14 bg-no-repeat  bg-logo bg-pos"
+            style={{ backgroundImage: "url(/img/instagramLogo.png)" }}
+          ></div>
+        </div>
+        <form className="flex items-center  flex-col">
           {children}
         </form>
       </div>
