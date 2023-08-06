350d5d0d79fdd35f61314a0a8937057ad2cc9571
Ali Karabulut <a.kbt6666@gmail.com>
2023-08-07 00:03:27 +0300

    icon update

---
+++ src/components/icons/menuIcon.tsx
@@ -0,0 +1,56 @@
+type MenuIconTypes = {
+  className?: string;
+};
+
+export default function MenuIcon({
+  className,
+  ...props
+}: MenuIconTypes) {
+  return (
+    <svg
+      {...{ className }}
+      {...props}
+      aria-label="Ana Sayfa"
+      color="rgb(0, 0, 0)"
+      fill="rgb(0, 0, 0)"
+      height="24"
+      role="img"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <line
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        x1="3"
+        x2="21"
+        y1="4"
+        y2="4"
+      ></line>
+      <line
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        x1="3"
+        x2="21"
+        y1="12"
+        y2="12"
+      ></line>
+      <line
+        fill="none"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        x1="3"
+        x2="21"
+        y1="20"
+        y2="20"
+      ></line>
+    </svg>
+  );
+}
---
+++ src/components/icons/menuIconFilled.tsx
@@ -0,0 +1,24 @@
+type MenuIconFilledTypes = {
+  className?: string;
+};
+
+export default function MenuIconFilled({
+  className,
+  ...props
+}: MenuIconFilledTypes) {
+  return (
+    <svg
+      {...{ className }}
+      {...props}
+      aria-label="Ana Sayfa"
+      color="rgb(0, 0, 0)"
+      fill="rgb(0, 0, 0)"
+      height="24"
+      role="img"
+      viewBox="0 0 24 24"
+      width="24"
+    >
+      <path d="M3.5 6.5h17a1.5 1.5 0 0 0 0-3h-17a1.5 1.5 0 0 0 0 3Zm17 4h-17a1.5 1.5 0 0 0 0 3h17a1.5 1.5 0 0 0 0-3Zm0 7h-17a1.5 1.5 0 0 0 0 3h17a1.5 1.5 0 0 0 0-3Z"></path>
+    </svg>
+  );
+}
